6f00dec0b66c4fbab5df8616bfcf72b33f830920
Fallencap <subham.saha.dev@gmail.com>
2025-10-15 09:06:44 +0530

    Making uitl function for url shortning

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"log"
+	"log/slog"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := "root:root@tcp(127.0.0.1):3306/url_shortner?charset=utf8mb4&parseTime=True&loc=Local"
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to DB:", err.Error())
+	}
+	
+	slog.Info("Database Connected Successfully")
+}
---
+++ utils/shortner.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ0123456789"
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func GenerateShortCode(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
